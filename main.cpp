6116982d4d97ca91e5344016711cd7a165725b30
TheCasProg <68574716+TheCasProg@users.noreply.github.com>
2020-09-03 20:33:56 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,17 +8,22 @@ using namespace std;
 const int WIDTH = 25;
 const int HEIGHT = 25;
 
+int snake_HeadX = WIDTH/2;
+int snake_HeadY = HEIGHT/2;
 
 void Game_Draw();
 void User_Input();
 void Game_Logic();
+int Game_Assets(int Draw_X, int Draw_Y, char * Obj_Icon);
+
+
 
 int main()
 {
-    char c_userIsAlive;
+    bool c_userIsAlive = false ;
     while (c_userIsAlive != true){
         Game_Draw();
-        Sleep(10);
+        Sleep(100);
         system("cls");
     }
     return 0;
@@ -26,23 +31,55 @@ int main()
 
 void Game_Draw()
 {
-    for (int i = 0; i != HEIGHT; i++){
-        if (i == 0 || i == HEIGHT - 1){
-            for (int j = 0; j < WIDTH; j++){
+    char Obj_Icon = ' ';
+    for (int i = 0; i != HEIGHT; i++)
+        {
+        if (i == 0 || i == HEIGHT - 1)
+        {
+            for (int j = 0; j < WIDTH; j++)
+            {
                 cout << "* ";
             }
             cout << endl;
         }
-        else {
-            for (int k = 0; k < WIDTH; k++){
-                if (k == 0 || k == WIDTH - 1){
+        else
+        {
+            for (int k = 0; k < WIDTH; k++)
+            {
+                if (k == 0 || k == WIDTH - 1)
+                {
                     cout << "* ";
                 }
-                else{
+                else if(Game_Assets(k, i, &Obj_Icon))
+                {
+                    cout << Obj_Icon << ' ';
+                }
+                else
+                {
                     cout << "  ";
                 }
             }
             cout << endl;
         }
     }
+}
+
+void Game_inputs()
+{
+
+}
+
+void Game_Logic()
+{
+
+}
+
+int Game_Assets(int Draw_X, int Draw_Y, char * OBJ_Icon)
+{
+    if (Draw_X == snake_HeadX && Draw_Y == snake_HeadY)
+    {
+        *OBJ_Icon = '@';
+        return 1;
+    }
+    return 0;
 }
