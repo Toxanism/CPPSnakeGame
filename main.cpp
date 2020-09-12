84e219e0ef7c81b4f9984fd36d10122e0b08e10d
TheCasProg <68574716+TheCasProg@users.noreply.github.com>
2020-09-13 00:44:30 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <Windows.h>
+#include <conio.h>
 
 using namespace std;
 
@@ -10,19 +11,20 @@ const int HEIGHT = 25;
 
 int snake_HeadX = WIDTH/2;
 int snake_HeadY = HEIGHT/2;
+char user_Input = ' ';
 
 void Game_Draw();
 void User_Input();
 void Game_Logic();
 int Game_Assets(int Draw_X, int Draw_Y, char * Obj_Icon);
-
-
+void Game_Collision(int Draw_X, int Draw_Y);
 
 int main()
 {
     bool c_userIsAlive = false ;
     while (c_userIsAlive != true){
         Game_Draw();
+        User_Input();
         Sleep(100);
         system("cls");
     }
@@ -64,9 +66,47 @@ void Game_Draw()
     }
 }
 
-void Game_inputs()
+void User_Input()
 {
-
+    if (kbhit())
+    {
+        user_Input = _getch();
+        switch(user_Input)
+        {
+        case 'a':
+            snake_HeadX--;
+            break;
+        case 'd':
+        snake_HeadX++;
+            break;
+        case 's':
+            snake_HeadY++;
+            break;
+        case 'w':
+            snake_HeadY--;
+            break;
+        default:
+            return;
+        }
+    }
+    else
+        switch(user_Input)
+        {
+        case 'a':
+            snake_HeadX--;
+            break;
+        case 'd':
+            snake_HeadX++;
+            break;
+        case 's':
+            snake_HeadY++;
+            break;
+        case 'w':
+            snake_HeadY--;
+            break;
+        default:
+            return;
+        }
 }
 
 void Game_Logic()
@@ -81,5 +121,11 @@ int Game_Assets(int Draw_X, int Draw_Y, char * OBJ_Icon)
         *OBJ_Icon = '@';
         return 1;
     }
+
     return 0;
+}
+
+void Game_Collision(int Draw_X, int Draw_Y)
+{
+
 }
